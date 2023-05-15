4f01decbc1d9d43fa0b3694e0ab841b024892d75
ArNiBu4221 <artem53142100@gmail.com>
2023-05-15 14:19:29 +0300

    Calendar was added
    The base datepicker was added to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { setupCalendar } from "v-calendar";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "../firebase/firebase.config";
@@ -12,5 +13,6 @@ const db = getFirestore(firebaseApp);
 const app = createApp(App);
 app.use(router).use(VueFire, { firebaseApp, modules: [] }).mount("#app");
 app.use(cors());
+app.use(setupCalendar, {});
 
 export { db };
