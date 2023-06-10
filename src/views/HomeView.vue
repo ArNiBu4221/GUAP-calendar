26a19b9e316f4db10ac2c7b3054da9ae9dacca7e
ArNiBu4221 <artem53142100@gmail.com>
2023-06-10 04:52:37 +0300

    New Event Logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,19 +4,25 @@
     <p class="title is-primary is-vcentered pt-6 is-size-1">
       Приветствую вас ГУАПчичи!
     </p>
-    <div class="columns">
-      <div
-        id="test"
-        v-for="i in 10"
-        :key="i"
-        class="column has-background-grey"
-      >
-        1
-      </div>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">Card header</p>
+        <button class="card-header-icon" aria-label="more options">
+          <span class="icon has-text-black">
+            <svg-icon type="mdi" :path="plusIcon"></svg-icon>
+          </span>
+        </button>
+      </header>
     </div>
   </div>
 </template>
 
+<script setup>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiPlusBoxOutline } from "@mdi/js";
+
+const plusIcon = mdiPlusBoxOutline;
+</script>
 <script>
 export default {
   name: "HomeView",
